e5b02b787bf09526bf93ba59a0bd817ec537c1f6
HR <89749037+hrquiz@users.noreply.github.com>
2021-11-28 15:42:32 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   socialImageSubtitle: 'Haikal Rahman',
 
   // social usernames (optional)
-  twitter: '',
+  twitter: 'hhaikalrahman',
   github: 'haikalmyid',
   linkedin: 'haikalrahman70',
 
