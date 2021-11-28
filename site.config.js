7cfa9277fd0adafa72a23aeb1027c4d3016b2b1a
HR <89749037+hrquiz@users.noreply.github.com>
2021-11-28 15:26:28 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'haikalmyid',
   linkedin: 'haikalrahman70',
 
   // default notion icon and cover images for site-wide consistency (optional)
