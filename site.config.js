bf812ca856a64299dc40a660f0fae1d465451058
HR <89749037+hrquiz@users.noreply.github.com>
2021-11-28 14:48:33 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Haikal Rahman',
+  domain: 'hrclass.unimed.in',
+  author: 'Haikal Rahman',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Materi Kuliah - FE Unimed',
+  socialImageTitle: 'Haikal Rahman',
+  socialImageSubtitle: 'Haikal Rahman',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
   github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  linkedin: 'haikalrahman70',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
